7215f7aa1eb3d535d9c8791c18c1ff0557019b35
Lucian Dragomir <lucian.dragomir@vodafone.com>
2020-05-11 03:36:30 +0300

    - add copy constructor for InspectContentResponse

--- src/main/java/org/digitalmind/signaturecartrige/dto/InspectContentResponse.java
+++ src/main/java/org/digitalmind/signaturecartrige/dto/InspectContentResponse.java
@@ -5,17 +5,14 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-@EqualsAndHashCode
+//@EqualsAndHashCode
 @ToString
 public class InspectContentResponse {
 
@@ -71,7 +68,52 @@ public class InspectContentResponse {
                 : null;
         this.privateVersion = inspectContentResponse.isPrivateVersion();
         this.privateFields = inspectContentResponse.getPrivateFields() != null ? new ArrayList<>(inspectContentResponse.getPrivateFields()) : null;
-        this.replaceFields(inspectContentResponse.getReplaceFields() != null ? new ArrayList<>(inspectContentResponse.getReplaceFields()) : null;
+        this.replaceFields = inspectContentResponse.getReplaceFields() != null ? new ArrayList<>(inspectContentResponse.getReplaceFields()) : null;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        InspectContentResponse that = (InspectContentResponse) o;
+        return signatureOk == that.signatureOk &&
+                privateVersion == that.privateVersion &&
+                Objects.equals(signatureMissingFields, that.signatureMissingFields) &&
+                Objects.equals(signatureAdditionalFields, that.signatureAdditionalFields) &&
+                mapEquals(signatureFieldDetails, that.signatureFieldDetails) &&
+                Objects.equals(privateFields, that.privateFields) &&
+                Objects.equals(replaceFields, that.replaceFields);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(signatureOk, signatureMissingFields, signatureAdditionalFields, mapHashCode(signatureFieldDetails), privateVersion, privateFields, replaceFields);
+    }
+
+    private boolean mapEquals(Map<String, float[]> map1, Map<String, float[]> map2) {
+        if (map1 == null && map2 == null) {
+            return true;
+        }
+        if (map1 == null || map2 == null) {
+            return false;
+        }
+        if (map1.size() != map2.size()) {
+            return false;
+        }
+        return map1.entrySet().stream()
+                .allMatch(e -> Arrays.equals(e.getValue(), map2.get(e.getKey())));
+    }
+
+    private int mapHashCode(Map<String, float[]> map) {
+        int h = 0;
+        Iterator<Map.Entry<String, float[]>> i = map.entrySet().iterator();
+        while (i.hasNext())
+            h += mapEntryHashCode(i.next());
+        return h;
+    }
+
+    private final int mapEntryHashCode(Map.Entry<String, float[]> entry) {
+        return Objects.hashCode(entry.getKey()) ^ Arrays.hashCode(entry.getValue());
     }
 
 }
